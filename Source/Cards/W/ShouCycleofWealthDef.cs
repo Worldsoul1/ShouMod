2780c74087dcaaa5e763c6903dedb819e1f73ddb
Worldsoul15 <Worldsoul895@gmail.com>
2025-02-09 16:14:01 -0500

    adding Cycle of Wealth

---
+++ Source/Cards/W/ShouCycleofWealthDef.cs
@@ -0,0 +1,57 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoLEntitySideloader.Attributes;
+using System.Collections.Generic;
+using ShouMod.Cards.Template;
+using LBoL.Core.Battle;
+using LBoL.Core;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Battle.Interactions;
+using LBoL.Core.Cards;
+using LBoL.Core.StatusEffects;
+using ShouMod.StatusEffects;
+
+namespace ShouMod.Cards
+{
+    public sealed class ShouCycleofWealthDef : ShouGemstoneReferenceCardTemplate
+    {
+
+
+        public override CardConfig MakeConfig()
+        {
+            CardConfig config = GetCardDefaultConfig();
+
+            config.Colors = new List<ManaColor>() { ManaColor.White };
+            config.Cost = new ManaGroup() { Any = 2, White = 2 };
+            config.UpgradedCost = new ManaGroup() { Any = 1, White = 1 };
+            config.Rarity = Rarity.Rare;
+
+            config.Type = CardType.Ability;
+            config.TargetType = TargetType.Self;
+
+            config.Value1 = 1;
+
+            //Keyword that doesn't add an effect to the card, but to add to the card's tooltips.
+
+            config.RelativeEffects = new List<string>() { nameof(ShouEndlessWealthSe) };
+            config.UpgradedRelativeEffects = new List<string>() { nameof(ShouEndlessWealthSe) };
+
+            config.Illustrator = "";
+
+            config.Index = CardIndexGenerator.GetUniqueIndex(config);
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(ShouCycleofWealthDef))]
+    public sealed class ShouCycleofWealth : SampleCharacterCard
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return BuffAction<ShouCycleofWealthSe>(base.Value1, 0, 0, 0, 0.2f);
+
+            //This is equivalent to:
+            //yield return new CastBlockShieldAction(Battle.Player, base.Block, base.Shield, BlockShieldType.Normal, true); 
+        }
+    }
+}
---
+++ Source/StatusEffects/ShouCycleofWealthSe.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Cards;
+using LBoL.Core.Randoms;
+using LBoL.Core.StatusEffects;
+using LBoL.Core.Units;
+using LBoLEntitySideloader.Attributes;
+using ShouMod.Cards;
+using System;
+using LBoL.Base.Extensions;
+
+namespace ShouMod.StatusEffects
+{
+    public sealed class ShouCycleofWealthSeDef : SampleCharacterStatusEffectTemplate
+    {
+        public override StatusEffectConfig MakeConfig()
+        {
+            StatusEffectConfig config = GetDefaultStatusEffectConfig();
+            config.HasLevel = true;
+            config.RelativeEffects = new List<string>() { nameof(TempFirepower), nameof(TempSpirit) };
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(ShouCycleofWealthSeDef))]
+    public sealed class ShouCycleofWealthSe : StatusEffect
+    {
+        public List<Type> Gemstones = new List<Type>
+        {
+            typeof(ShouAmber),
+            typeof(ShouDiamond),
+            typeof(ShouEmerald),
+            typeof(ShouOnyx),
+            typeof(ShouOpal),
+            typeof(ShouPearl),
+            typeof(ShouRuby),
+            typeof(ShouSapphire),
+        };
+        protected override void OnAdded(Unit unit)
+        {
+            base.ReactOwnerEvent<CardUsingEventArgs>(base.Battle.CardPlayed, new EventSequencedReactor<CardUsingEventArgs>(this.OnCardPlayed));
+        }
+
+        // Token: 0x060000C6 RID: 198 RVA: 0x000036D2 File Offset: 0x000018D2
+        private IEnumerable<BattleAction> OnCardPlayed(CardUsingEventArgs args)
+        {
+            if (args.Card is ShouGemstoneCard)
+            {
+                this.Gemstones.Shuffle(base.GameRun.BattleCardRng);
+                yield return new ExileCardAction(args.Card);
+                yield return new AddCardsToDiscardAction(Library.CreateCard(this.Gemstones[0]));
+            }
+            yield break;
+        }
+    }
+}
