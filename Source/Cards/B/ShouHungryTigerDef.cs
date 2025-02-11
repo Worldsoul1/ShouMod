e6c89cdaca1e870b05f4d7c115c7e96deead30a3
Worldsoul15 <Worldsoul895@gmail.com>
2025-02-11 16:57:41 -0500

    added two new cards: Hungry Tiger and Protective Money

---
+++ Source/Cards/B/ShouHungryTigerDef.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Cards;
+using LBoL.Core.StatusEffects;
+using LBoLEntitySideloader.Attributes;
+using ShouMod.Cards.Template;
+using ShouMod.GunName;
+
+namespace ShouMod.Cards
+{
+    // Token: 0x020002AD RID: 685
+    public sealed class ShouHungryTigerDef : SampleCharacterCardTemplate
+    {
+        public override CardConfig MakeConfig()
+        {
+            CardConfig config = GetCardDefaultConfig();
+            config.GunName = GunNameID.GetGunFromId(400);
+            //If IsPooled is false then the card cannot be discovered or added to the library at the end of combat.
+            config.IsPooled = false;
+
+            config.Colors = new List<ManaColor>() { ManaColor.Black };
+            config.Cost = new ManaGroup() { Any = 2, Black = 1 };
+            config.Rarity = Rarity.Uncommon;
+
+            config.Type = CardType.Attack;
+            config.TargetType = TargetType.SingleEnemy;
+
+            config.Damage = 18;
+            config.UpgradedDamage = 21;
+
+            config.Value1 = 10;
+
+            config.Keywords = Keyword.Exile;
+            //Setting Upgrading Keyword only provides the keyword when the card is upgraded.    
+            config.UpgradedKeywords = Keyword.Exile | Keyword.Accuracy;
+
+            config.Illustrator = "Radal";
+
+            config.Index = CardIndexGenerator.GetUniqueIndex(config);
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(ShouHungryTigerDef))]
+    public sealed class ShouHungryTiger : SampleCharacterCard
+    {
+        // Token: 0x06000A39 RID: 2617 RVA: 0x000150EC File Offset: 0x000132EC
+        protected override void OnEnterBattle(BattleController battle)
+        {
+            base.ReactBattleEvent<DieEventArgs>(base.Battle.EnemyDied, new EventSequencedReactor<DieEventArgs>(this.OnEnemyDied));
+        }
+
+        // Token: 0x06000A3A RID: 2618 RVA: 0x0001510B File Offset: 0x0001330B
+        private IEnumerable<BattleAction> OnEnemyDied(DieEventArgs args)
+        {
+            if (args.DieSource == this && !args.Unit.HasStatusEffect<Servant>())
+            {
+                yield return HealAction(base.Value1);
+            }
+            yield break;
+        }
+    }
+}
---
+++ Source/Cards/Mult/ShouProtectiveMoneyDef.cs
@@ -0,0 +1,63 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoLEntitySideloader.Attributes;
+using System.Collections.Generic;
+using ShouMod.Cards.Template;
+using LBoL.Core.Battle;
+using LBoL.Core;
+using LBoL.Core.Battle.BattleActions;
+using ShouMod.StatusEffects;
+using LBoL.Core.Battle.Interactions;
+using LBoL.Core.Cards;
+
+namespace ShouMod.Cards
+{
+    public sealed class ShouProtectiveMoneyDef : SampleCharacterCardTemplate
+    {
+
+
+        public override CardConfig MakeConfig()
+        {
+            CardConfig config = GetCardDefaultConfig();
+
+            config.Colors = new List<ManaColor>() { ManaColor.White, ManaColor.Red };
+            config.Cost = new ManaGroup() { Any = 1, Hybrid = 2, HybridColor = 2 };
+            config.UpgradedCost = new ManaGroup() { Any = 2, Hybrid = 1, HybridColor = 2 };
+            config.Rarity = Rarity.Uncommon;
+
+            config.Type = CardType.Defense;
+            config.TargetType = TargetType.Self;
+
+            config.Shield = 15;
+            config.UpgradedShield = 20;
+
+            config.Value1 = 2;
+            config.UpgradedValue1 = 2;
+
+            config.Keywords = Keyword.Exile;
+            config.UpgradedKeywords = Keyword.Exile;
+            //Keyword that doesn't add an effect to the card, but to add to the card's tooltips.
+            config.RelativeKeyword = Keyword.Shield;
+            config.UpgradedRelativeKeyword = Keyword.Shield;
+
+            config.Illustrator = "";
+
+            config.Index = CardIndexGenerator.GetUniqueIndex(config);
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(ShouProtectiveMoneyDef))]
+    public sealed class ShouProtectiveMoney : SampleCharacterCard
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            int ExtraMoney = 0;
+            yield return DefenseAction(true);
+            ExtraMoney = base.Value1 * base.Battle.HandZone.Count;
+            yield return new GainMoneyAction(ExtraMoney, SpecialSourceType.None);
+            //This is equivalent to:
+            //yield return new CastBlockShieldAction(Battle.Player, base.Block, base.Shield, BlockShieldType.Normal, true); 
+        }
+    }
+}
