8dc8993e4dde746bf709bfffff1706857ea8c5c3
Worldsoul15 <Worldsoul895@gmail.com>
2025-02-10 21:59:43 -0500

    new card: Divine Protector

---
+++ Source/Cards/Mult/ShouDivineProtectorDef.cs
@@ -0,0 +1,58 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoLEntitySideloader.Attributes;
+using System.Collections.Generic;
+using ShouMod.Cards.Template;
+using LBoL.Core.Battle;
+using LBoL.Core;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Battle.Interactions;
+using LBoL.Core.Cards;
+using LBoL.Core.StatusEffects;
+using ShouMod.StatusEffects;
+
+namespace ShouMod.Cards
+{
+    public sealed class ShouDivineProtectorDef : SampleCharacterCardTemplate
+    {
+
+
+        public override CardConfig MakeConfig()
+        {
+            CardConfig config = GetCardDefaultConfig();
+
+            config.Colors = new List<ManaColor>() { ManaColor.White, ManaColor.Red };
+            config.Cost = new ManaGroup() { Any = 3, White = 1, Red = 1};
+            config.UpgradedCost = new ManaGroup() { Any = 2, Hybrid = 1, HybridColor = 2 };
+            config.Rarity = Rarity.Rare;
+
+            config.Type = CardType.Ability;
+            config.TargetType = TargetType.Self;
+
+            config.Value1 = 1;
+            config.UpgradedValue1 = 1;
+
+            //Keyword that doesn't add an effect to the card, but to add to the card's tooltips.
+
+            config.RelativeEffects = new List<string>() { nameof(ShouDivineProtectorSe) };
+            config.UpgradedRelativeEffects = new List<string>() { nameof(ShouDivineProtectorSe) };
+
+            config.Illustrator = "";
+
+            config.Index = CardIndexGenerator.GetUniqueIndex(config);
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(ShouDivineProtectorDef))]
+    public sealed class ShouDivineProtector : SampleCharacterCard
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return BuffAction<ShouDivineProtectorSe>(base.Value1, 0, 0, 0, 0.2f);
+            
+            //This is equivalent to:
+            //yield return new CastBlockShieldAction(Battle.Player, base.Block, base.Shield, BlockShieldType.Normal, true); 
+        }
+    }
+}
