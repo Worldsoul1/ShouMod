3f856c2fbaa5cd51a47caedd4e2de7d70447f364
Worldsoul15 <Worldsoul895@gmail.com>
2025-03-19 18:50:15 -0400

    New art for nine cards, and a new card

---
+++ Source/Cards/W/ShouTimeisMoneyDef.cs
@@ -0,0 +1,62 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoLEntitySideloader.Attributes;
+using System.Collections.Generic;
+using ShouMod.Cards.Template;
+using LBoL.Core.Battle;
+using LBoL.Core;
+using LBoL.Core.StatusEffects;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.EntityLib.Cards;
+using LBoL.EntityLib.StatusEffects.ExtraTurn;
+using ShouMod.StatusEffects;
+
+namespace ShouMod.Cards
+{
+    public sealed class ShouTimeisMoneyDef : ShouGemstoneReferenceCardTemplate
+    {
+        public override CardConfig MakeConfig()
+        {
+            CardConfig config = GetCardDefaultConfig();
+            config.Colors = new List<ManaColor>() { ManaColor.White };
+            config.Cost = new ManaGroup() { Any = 3, White = 2 };
+            config.UpgradedCost = new ManaGroup() { Any = 1, White = 2 };
+            config.Rarity = Rarity.Rare;
+
+            config.Type = CardType.Skill;
+            config.TargetType = TargetType.Nobody;
+
+            //Mana config for the Time Limit
+            config.Mana = new ManaGroup() { Any = 1 };
+
+            config.RelativeEffects = new List<string>() { nameof(TimeIsLimited) };
+            config.UpgradedRelativeEffects = new List<string>() { nameof(TimeIsLimited) };
+
+            config.Value1 = 5;
+            config.Value2 = 8;
+
+            config.Illustrator = "";
+
+            config.Index = CardIndexGenerator.GetUniqueIndex(config);
+            return config;
+        }
+    }
+
+    [EntityLogic(typeof(ShouTimeisMoneyDef))]
+    //TimeStopCards inhehit from LimitedStopTimeCard instead of Card
+    public sealed class ShouTimeisMoney : LimitedStopTimeCard
+    {
+        protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+        {
+            yield return new ApplyStatusEffectAction<ShouTimeisMoneySe>(base.Battle.Player, base.Value1, 0, 0, 0, 0.2f);
+            yield return base.BuffAction<ExtraTurn>(1, 0, 0, 0, 0.2f);
+            yield return new RequestEndPlayerTurnAction();
+            if (base.Limited)
+            {
+                yield return base.DebuffAction<TimeIsLimited>(base.Battle.Player, 1, 0, 0, 0, true, 0.2f);
+            }
+            yield break;
+        }
+    }
+}
+
---
+++ Source/StatusEffects/ShouTimeisMoneySe.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using LBoL.Base.Extensions;
+using LBoL.ConfigData;
+using LBoL.Core;
+using LBoL.Core.Battle;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.Core.Cards;
+using LBoL.Core.Units;
+using LBoL.EntityLib.StatusEffects.ExtraTurn;
+using ShouMod.Cards;
+
+namespace ShouMod.StatusEffects
+{
+    // Token: 0x02000081 RID: 129
+    public sealed class ShouTimeisMoneySeDef : SampleCharacterStatusEffectTemplate
+    {
+        public override StatusEffectConfig MakeConfig()
+        {
+            StatusEffectConfig statusEffectConfig = GetDefaultStatusEffectConfig();
+            statusEffectConfig.HasLevel = true;
+            return statusEffectConfig;
+        }
+    }
+
+    public sealed class ShouTimeisMoneySe : ExtraTurnPartner
+    {
+        public List<Type> Gemstones = new List<Type>
+        {
+            typeof(ShouAmber),
+            typeof(ShouDiamond),
+            typeof(ShouEmerald),
+            typeof(ShouOnyx),
+            typeof(ShouOpal),
+            typeof(ShouPearl),
+            typeof(ShouRuby),
+            typeof(ShouSapphire),
+        };
+        // Token: 0x060001E1 RID: 481 RVA: 0x00005C34 File Offset: 0x00003E34
+        protected override void OnAdded(Unit unit)
+        {
+            base.ThisTurnActivating = false;
+            base.HandleOwnerEvent<UnitEventArgs>(base.Battle.Player.TurnStarting, delegate (UnitEventArgs _)
+            {
+                if (base.Battle.Player.IsExtraTurn && !base.Battle.Player.IsSuperExtraTurn && base.Battle.Player.GetStatusEffectExtend<ExtraTurnPartner>() == this)
+                {
+                    base.ThisTurnActivating = true;
+                }
+            });
+            base.HandleOwnerEvent<CardEventArgs>(base.Battle.Predraw, new GameEventHandler<CardEventArgs>(this.OnPredraw));
+            base.ReactOwnerEvent<UnitEventArgs>(base.Owner.TurnStarted, new EventSequencedReactor<UnitEventArgs>(this.OnOwnerTurnStarted));
+        }
+
+        // Token: 0x060001E2 RID: 482 RVA: 0x00005CA4 File Offset: 0x00003EA4
+        private IEnumerable<BattleAction> OnOwnerTurnStarted(UnitEventArgs args)
+        {
+            if (base.ThisTurnActivating)
+            {
+                this.Gemstones.Shuffle(base.GameRun.BattleCardRng);
+                List<Card> list = new List<Card>();
+                for (int i = 0; i < Level; i++)
+                {
+                    list.Add(Library.CreateCard(this.Gemstones[i]));
+                }
+                yield return new AddCardsToHandAction(list);
+                yield return new RemoveStatusEffectAction(this, true, 0.1f);
+            }
+            yield break;
+        }
+
+        // Token: 0x060001E3 RID: 483 RVA: 0x00005CB4 File Offset: 0x00003EB4
+        private void OnPredraw(CardEventArgs args)
+        {
+            if (base.ThisTurnActivating && args.Cause == ActionCause.TurnStart)
+            {
+                args.CancelBy(this);
+            }
+        }
+    }
+}
